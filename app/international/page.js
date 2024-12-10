7dfe909981d0cbeaa62965ea4d4f6aa15c871c22
UltramateDiamond <166836992+UltramateDiamond@users.noreply.github.com>
2024-12-09 20:12:10 -0700

    Update page.js

--- app/international/page.js
+++ app/international/page.js
@@ -3,7 +3,7 @@ export default function InternationalNews() {
 
     return(
         <main>
-            <img src="">Test </img>
+            <img src="https://en.wikipedia.org/wiki/Kirby_(character)"/>
             <button>Learn More</button>
         </main>
     );
