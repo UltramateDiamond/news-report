dced879efde04e41fd986417e2466e7edf38f2c9
UltramateDiamond <166836992+UltramateDiamond@users.noreply.github.com>
2024-12-09 20:52:08 -0700

    Needs flexboxes

--- app/international/page.js
+++ app/international/page.js
@@ -3,8 +3,64 @@ export default function InternationalNews() {
 
     return(
         <main>
-            <img src="https://en.wikipedia.org/wiki/Kirby_(character)"/>
-            <button>Learn More</button>
+            <div className="flex-container">
+                <div className="border border-blue-900">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+                <div className="container mx-auto p-4 flex w-full max-w-md bg-purple-100 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <img src="https://en.wikipedia.org/wiki/Kirby_(character)" alt="Kirby" />
+                    <p>Kirby copypasta is a massive international threat to North America. Everytime that this copypasta appears, more than a million people who view it dies. Please be responsible and turn off your internet devices until the threat disappears.</p>
+                    <a href="https://www.reddit.com/r/Kirby/comments/12afsvj/getting_drag_race_when_you_have_hydra_be_like/">
+                        <button>Learn More</button>
+                    </a>
+                </div>
+            </div>
         </main>
     );
 }
